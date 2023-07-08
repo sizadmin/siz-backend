8484802ac1c57e774a0194833e46f9ddb7fedf1b
Yuvraj Kolambe <ykolambe123@gmail.com>
2023-07-08 19:33:55 +0400

    order saved in mongo

--- controllers/v1/webhooks/index.ts
+++ controllers/v1/webhooks/index.ts
@@ -12,6 +12,7 @@ const fetchShopifyOrderUsingWebhook = async (req: any, res: any) => {
         const { body } = req;
         console.log(body);
         saveOrderInDb(body);
+        sendWhatsappMessageToRenter(body);
         res.status(200).json({
             success: true,
             message: "Shopify products fetched successfully.",
@@ -25,6 +26,33 @@ const fetchShopifyOrderUsingWebhook = async (req: any, res: any) => {
             .status(500)
             .json({ success: false, error: "Failed to fetch shopify products" });
     }
+}
+
+
+const sendWhatsappMessageToRenter = async (body: any ) => {
+    //const renterPhoneNumber = body.phone ;
+    const axios = require('axios');
+let data = '{\n    "messaging_product": "whatsapp",\n    "to": "971561114006",\n    "type": "template",\n    "template": {\n       "name": "order_confirmed ",\n       "language": {\n           "code": "en_US",\n           "policy": "deterministic"\n       },\n       "components": [\n          {\n           "type": "body",\n           "parameters": [\n               {\n                   "type": "text",\n                   "text": "Annabel"\n               },\n                {\n                   "type": "text",\n                   "text": "Freya Dress"\n               }, {\n                   "type": "text",\n                   "text": "07 July 2023"\n               } ]\n         },\n         {\n          "type": "button",\n          "sub_type": "url",\n           "index": "0",\n          "parameters": [\n            {\n              "type": "text",\n              "text": "23493282245"\n            }\n          ]\n        }\n\n       ]\n    }\n}';
+
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: 'https://graph.facebook.com/v17.0/105942389228737/messages',
+  headers: { 
+    'Content-Type': 'application/json', 
+    'Authorization': 'Bearer EAAIl8Exy9ZCMBAPg1zMfZB29P1CoVwc808VMnMdinUbNZBtZBwuBQn9B2bajxejWbLkRECigTuWifuqLMcNk3dpJOgoWW1Xp1Mcnz1rZBHa9hn72EV3GiS4qZBVin6l6tvi7oVEofrSOumn3Kf6PILkfcbRUy5MaKEGWL9OLZB9rfTzkcAmJ46S3IR7i7fhFy1yrRX3pTqHNe26wS6aImhGkpBvbxKtzZBQZD'
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
+
 }
 
 const saveOrderInDb = async (body :  any) => {
