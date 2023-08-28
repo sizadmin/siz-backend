951868ee426fef23b080ac9e21c4aacfc90f5dcb
Yuvraj Kolambe <ykolambe123@gmail.com>
2023-08-28 16:18:31 +0400

    delivery reminders

--- types/order.ts
+++ types/order.ts
@@ -7,5 +7,16 @@ export interface IOrder extends Document {
     email:string;
     phone_number:string;
     order_details:Schema.Types.Mixed;
-
+    order_items :Schema.Types.Mixed;
+    lender_name:string;
+    lender_address:string;
+    lender_phone_call :string;
+    lender_phone_whatsapp :string;
+    renter_phone_number :string;
+    rental_start_date :string;
+    rental_end_date :string;
+    renter_name :string;
+    rental_duration :string; 
+    rental_piece_name :string;
+    backup_piece :string;
 }
