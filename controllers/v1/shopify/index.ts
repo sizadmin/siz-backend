e1fa87d38493984d56b318cf08132d53040f98d1
sizdevadmin <148195962+sizdevadmin@users.noreply.github.com>
2024-04-20 16:55:20 +0400

    aaaa

--- controllers/v1/shopify/index.ts
+++ controllers/v1/shopify/index.ts
@@ -63,7 +63,7 @@ const fetchShopifyOrder = async (req: any, res: any) => {
         } else {
           console.log("Influencer Tag not found.");
         }
-        let key = (order.line_items[0]) ? order.line_items[0].properties[0].name : "";
+        let key = (order.line_items) ? order.line_items[0].properties[0].name : "";
         if (key == "Date") {
           dateString = order.line_items[0].properties[0].value;
         }
