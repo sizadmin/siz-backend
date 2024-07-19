2cfd5ff7498c432159a964630cd5d4274c5328f9
sizadmin <heysiz.ae@gmail.com>
2024-07-19 22:28:59 +0400

    message

--- routes/webhooks.ts
+++ routes/webhooks.ts
@@ -5,7 +5,7 @@ const router: Router = Router();
 
 
 router.post('/webhooks/orders/create', fetchShopifyOrderUsingWebhook);
-router.post('/webhooks/listen/whatsapp', listenRepliesFromWebhook);
+router.post('/webhooks/verify/whatsapp', listenRepliesFromWebhook);
 router.get('/webhooks/verify/whatsapp', verifyTokenWhatsapp);
 
 
